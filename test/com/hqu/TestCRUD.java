61d4e3478a6f2d97cfcac32a3c90f4b7fce0e2c9
suzhida <jerome_s@qq.com>
2016-08-25 09:05:20 +0800

    设置实体类别名

--- test/com/hqu/TestCRUD.java
+++ test/com/hqu/TestCRUD.java
@@ -1,14 +1,10 @@
 package com.hqu;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
